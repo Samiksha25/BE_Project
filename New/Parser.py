0e13fce0731b4e61b6293239727cc4e627607452
Samiksha25 <33507456+Samiksha25@users.noreply.github.com>
2017-12-21 14:01:20 +0530

    Add files via upload

---
+++ New/Parser.py
@@ -0,0 +1,342 @@
+import ply.yacc as yacc
+import sys
+arg1 = sys.argv[1]        # For taking input from command line.
+
+# Get the token map from the lexer.  This is required.
+from Scanner import tokens
+from astgenerator import *
+from TreeTraversal import *
+
+precedence = (
+	('left', '+', '-'),
+)
+#defines the starting grammar rule (top level rule)
+start = 'program'
+
+#handle empty productions
+def p_empty(p):
+    'empty :'
+    pass
+
+#following is the grammar
+def p_program(p):
+	"program : variable_declarations procedure_definition_list"
+	p[0]=program_grammar(p[1],p[2])
+	print ("Sucessfully parsed")
+	
+def p_proceduredefn_list(p):
+	''' procedure_definition_list : procedure_definition  procedure_definition_list
+								| empty'''
+	if len(p)==3:
+		print("VarName\tlevel\tscope")
+		for i in range(len(symbolTable)):
+			print(symbolTable[i].var,"\t",symbolTable[i].level,"\t",symbolTable[i].scope)    
+
+		p[0]= procedure_defination_list_grammar(p[2],p[1])
+
+		
+	
+def p_proceduredefn(p):
+	"""procedure_definition : NAME '(' def_parameters ')' '{'  variable_declarations ex_statement_list return_stat '}' 
+	"""
+	global level
+	if len(p)==10:
+		p[0]=procedure_defination_grammar(p[1],p[3],p[6],p[7],p[8])
+	level=0
+
+	
+def p_parametersfordef(p):
+	''' def_parameters : VAR ppointer_var def_parameter_list
+					| empty  '''
+	global level
+	if len(p)==4:
+		p[0]=parameters_for_def(p[2],p[3])
+	level=0
+	
+def p_defparameterlist(p):
+	'''def_parameter_list : ',' def_parameters
+						| empty '''
+	if len(p) == 3:
+		p[0] = def_parameter_list_grammar(p[2])
+
+def p_parameterforcall(p):
+	'''	call_parameters : ppointer_var call_parameter_list
+						| empty  '''
+	global level
+	if len(p) == 3:
+		p[0]=parameters_for_call(p[1], p[2])
+	level=0
+	
+def p_callparameterlist(p):
+	'''call_parameter_list : ',' call_parameters
+						| empty '''
+	if len(p)==3:
+		p[0]=call_parameter_list_grammar(p[2])
+    
+def p_variable_declarations(p):
+	'''variable_declarations : variable_declaration_list'''
+	global scope
+	
+	scope=scope+1
+	if len(p)==2:
+		p[0]=variable_declarations_grammar(p[1])
+
+def p_variabledeclarationlist(p):
+	''' variable_declaration_list : empty
+				  | variable_declaration variable_declaration_list '''
+	if len(p)==3:
+		p[0]=variable_dec_stat(p[2],p[1])
+						 
+def p_variabledeclaration(p):
+	''' variable_declaration : VAR var_list ';'
+							 '''
+	p[0]=var_variable_list(p[2])
+	
+def p_varlistpointer(p):
+	'''var_list : ppointer_var
+		| var_list  ',' ppointer_var '''
+	global level
+	global varCount
+	global multipleVar
+	global scope
+	flag=0
+	length=len(symbolTable)-1
+	if len(symbolTable)==0:
+		varEntry = SymbolEntry(varnames[varCount-1], level,scope)
+		symbolTable.append(varEntry)
+	else:
+		if varnames[varCount-1] in symbolTable[length].var:
+			print ("Multiple declarations of ",multipleVar)
+			flag=1
+		
+		if (flag != 1):
+			varEntry= SymbolEntry(varnames[varCount-1], level,scope)
+			symbolTable.append(varEntry)
+			flag=0
+	level=0
+	if len(p)==2:
+		p[0]=var_list_ppointer(p[1])
+	else:
+		p[0]=var_list_ppointerlist(p[1],p[3])
+		
+def p_varlist(p):
+	'''var_list : variable
+	     | var_list ',' variable '''
+	global level
+	global varCount
+	global scope
+	flag=0
+	global multipleVar
+	length=len(symbolTable)-1
+	if len(symbolTable)==0:
+		varEntry = SymbolEntry(varnames[varCount-1], level,scope)
+		symbolTable.append(varEntry)
+	else: 
+		if varnames[varCount-1] in symbolTable[length].var :
+				flag=1
+
+		if (flag != 1):
+			varEntry = SymbolEntry(varnames[varCount-1], level,scope)
+			symbolTable.append(varEntry)
+			flag=0
+			
+	level=0
+	if len(p)==2:
+		p[0]=var_list_variable(p[1])
+	else:
+		p[0]=var_list_variablelist(p[1],p[3])
+
+def p_pointervariable(p):
+	''' pointer_variable :  POINTER_OP variable''' 
+	p[0]=pointer_op_var(p[2])
+
+def p_ppointervariable(p):
+	'''ppointer_var : POINTER_OP ppointer_var
+			 | pointer_variable  '''
+	global interVar
+	global varCount
+	global level
+	if len(p)==2:
+		p[0]=p[1]
+		level+=1
+		p[0]=ppointer_var(p[1])
+	if len(p)==3:
+		level +=1
+		p[0]=ppointer_var_list(p[2])
+	
+def p_addrvariable(p):
+	'''addr_var : ADDRESS_OP variable'''
+	p[0]=addr_variableclass(p[2])
+		
+def p_exstatlist(p):
+	''' ex_statement_list : empty
+				| statement ex_statement_list  '''
+	if len(p)==3:
+		p[0]=statement_list(p[2],p[1])
+	
+	
+def p_stmt(p):
+	''' statement : assignment_statement'''
+	p[0]=assignment_statement_list(p[1])
+	
+def p_stmt_cond_goto(p):
+	'''statement :  cond_goto'''
+	p[0]=cond_goto_stat(p[1])
+
+def p_stmt_uncond_goto(p):
+	'''statement : uncond_goto'''
+	p[0]=uncond_goto_stat(p[1])
+	
+def p_stmt_use_stat	(p):
+	'''statement : use_stat'''
+	p[0]=use_stat(p[1])
+	
+	    	       
+def p_stmtlabel(p):
+	''' statement : label'''
+	p[0]=label_stat(p[1])
+
+def p_stmt_procedure_call(p):
+	'''statement : procedure_call'''
+	p[0]=procedure_call_stat(p[1])
+  
+def p_asgnstmt(p):
+	''' assignment_statement : expression_term ASSIGN_OP arith_expression ';' '''
+	global interVar
+	p[0]=assign(p[1],p[3])
+							
+def p_arithexpr(p):
+	''' arith_expression : arith_expression '+' arith_expression
+				| arith_expression '-' arith_expression
+				| expression_term '''
+	global interVar
+	if len(p)==2:
+		p[0]=arithmetic_term_exp(p[1])
+	
+	if len(p)==4:
+		p[0]=arithmetic_term_binop(p[1],p[2],p[3])
+    		
+
+def p_exprtermvar(p):
+	'''expression_term : variable'''
+	p[0]=exp_var(p[1])
+	
+
+def p_exprtermpointervar(p):
+	'''expression_term : ppointer_var'''
+	global level
+	p[0]=exp_ppointer_var(p[1])
+	level=0
+
+def p_exprtermaddrvar(p):
+	'''expression_term : addr_var'''
+	p[0]=exp_addr_var(p[1])
+
+def p_exprtermconstant(p):
+	'''expression_term : constant '''
+	p[0]=exp_constant(p[1])
+
+def p_exprtermprocedure(p):
+	'''expression_term : procedure_call'''
+	p[0]=exp_procedure_call(p[1])
+
+def p_condgoto(p):
+	''' cond_goto : IF '(' ')' if_cond_goto '''
+	p[0]=if_goto_grammer(p[4])
+	
+def p_ungoto(p):
+	''' uncond_goto : GOTO label '''
+	p[0]=uncond_goto_label(p[2])
+	
+def p_ifgoto(p):
+	'''if_cond_goto : GOTO label '''
+	p[0]=goto_label(p[2])
+
+def p_procedurecall(p): 
+    ''' procedure_call : NAME '(' call_parameters ')' ';' '''
+    p[0] = calling_procedure(p[1],p[3])
+	
+def p_var(p):
+	''' variable : NAME'''			
+	global varCount
+	global multipleVar
+	varflag=0	
+	flag=0
+	for entry in range(0,len(symbolTable)-1):
+		if p[1]==varnames[entry] and scope == symbolTable[entry].scope:
+				multipleVar=p[1]
+				flag=1
+				break
+	if (flag == 0):
+		varnames.append(p[1])
+		varCount+=1
+		flag=0
+
+	p[0]=var_name(p[1])
+
+def p_arr_var(p):
+	''' variable : arr_var'''	
+	global varCount
+	global multipleVar
+	p[0]=p[1]
+	if p[1] in varnames:
+		multipleVar=p[1]
+		pass
+	else:
+		varnames.append(p[1]) 
+		varCount+=1
+   
+def p_arrayvar(p):
+    ''' arr_var : NAME '[' NUM ']' '''		 
+    p[0]=p[1]
+	
+def p_cons(p):
+	''' constant : NUM '''
+	p[0]=constant_num(p[1])
+	
+				 
+def p_returnppointer(p):
+	''' return_stat : RETURN ppointer_var ';' '''
+	global level
+	level=0
+	p[0]=return_ppointer_var(p[2])
+	
+def p_return_addrvar(p):
+	''' return_stat : RETURN addr_var ';' '''
+	global level
+	level=0
+	p[0]=return_variable(p[2])
+
+def p_return_var(p):
+	''' return_stat : RETURN variable ';' '''
+	global level
+	level=0
+	p[0]=return_variable(p[2])
+
+def p_returnempty(p):
+	''' return_stat : empty '''
+
+def p_usestat(p):
+	''' use_stat : USE '(' ppointer_var ')' ';' '''
+	global level
+	level=0
+	p[0]=use_pointer(p[3])
+	
+				
+def p_label(p):
+    ''' label : '<' BB NUM '>' ':' '''
+    p[0]=label_num(p[3])
+				
+#for error handling
+def p_error(p):
+    print("Syntax error at '%s'  '%d' " %(p.value,p.lineno))
+
+#import ply.yacc as yacc
+parser = yacc.yacc()
+
+with open(arg1, 'r') as myfile:
+    data=myfile.read()
+
+result= parser.parse(data)
+
+tree_traversal(result)
---
+++ New/TreeTraversal.py
@@ -0,0 +1,323 @@
+class node:
+	def __init__(self,label,data,pred,succ):
+		self.label=label
+		self.data=data
+		self.pred=pred
+		self.succ=succ
+		
+global labels
+labels=0
+
+arr=list()
+
+Head=node(0,0,None, None)
+arr.append(Head)
+#print ("??!!!!!!!!!!!!??????")
+print (arr[0].data)
+
+class tree_traversal():
+	def ppointer_var_traversal(self,ppointer_variable):
+		#print("going in funct")
+		self.pointers ='*'
+		while(ppointer_variable):
+			try:
+				pointer_variable = ppointer_variable.pointer_variable_obj.variable_obj.name
+				#print(self.pointers,pointer_variable)
+				if self.flag_p==0:
+					self.variables = self.variables + '%s' %self.pointers + '%s' %pointer_variable 
+				else:
+					self.statement = self.statement + '%s' %self.pointers + '%s' %pointer_variable 
+					if(self.onestmt == ""):
+						self.onestmt = self.onestmt + '%s' %self.pointers + '%s' %pointer_variable
+						self.o_flag=1
+
+					elif(self.onestmt != "" and self.o_flag==1):
+						self.onestmt = self.onestmt + '%s' %self.pointers + '%s' %pointer_variable
+						global labels
+						labels=labels+1
+						node1=node(labels,self.onestmt,(labels-1), (labels+1))
+						arr.append(node1)
+						print ("-------------------")
+						print ("Current Label",arr[labels].label)
+						print ("DATA : ",arr[labels].data)
+						print ("Predeccesor Label",arr[labels].pred)
+						print ("Successor Label",arr[labels].succ)
+						print ("-------------------")
+						self.onestmt=""
+						self.o_flag=0
+
+			except:
+				pass
+			try:
+				ppointer_variable = ppointer_variable.ppointer_variable_obj
+				self.pointers ='*%s' %self.pointers
+				
+			except:
+				break
+
+	def def_parameters_traversal(self, def_parameters):
+		while(def_parameters):
+			try:
+				self.ppointer_var_traversal(def_parameters.ppointer_variable_obj)
+			except:
+				pass
+			try:
+				def_parameters=def_parameters.def_parameter_list_obj.def_parameters_obj	
+			except:
+				break
+	
+
+	def expression_term_traversal(self, expression_term):
+		try:
+			variable = expression_term.variable_obj.name 
+			self.statement = self.statement + variable
+			if(self.onestmt == ""):
+				self.onestmt = self.onestmt + variable
+				self.o_flag=1
+
+			elif(self.onestmt != "" and self.o_flag==1):
+				self.onestmt = self.onestmt + variable
+				global labels
+				labels=labels+1
+				node1=node(labels,self.onestmt,(labels-1), (labels+1))
+				arr.append(node1)
+				print ("-------------------")
+				print ("Current Label",arr[labels].label)
+				print ("DATA : ",arr[labels].data)
+				print ("Predeccesor Label",arr[labels].pred)
+
+				print ("Successor Label",arr[labels].succ)
+
+				print ("-------------------")
+				self.onestmt=""
+				self.o_flag=0
+				
+		except:
+			pass
+		try:
+			self.ppointer_var_traversal(expression_term.ppointer_variable_obj)
+		except:
+			pass
+		try:
+			global labels
+			addr_var=expression_term.addr_variable_obj.variable_obj.name
+			self.statement = self.statement + " & %s" %addr_var
+			self.onestmt=self.onestmt+" & %s" %addr_var
+			labels=labels+1
+			node1=node(labels,self.onestmt,(labels-1), (labels+1))
+			arr.append(node1)
+			print ("-------------------")
+			print ("Current Label",arr[labels].label)
+			print ("DATA : ",arr[labels].data)
+			print ("Predeccesor Label",arr[labels].pred)
+
+			print ("Successor Label",arr[labels].succ)
+			print ("-------------------")
+			self.onestmt=""
+		except:
+			pass
+		try:
+			constant= expression_term.constant_obj.num
+			self.statement = self.statement + " %s" %constant
+		
+		except:
+			pass
+		
+	def label_traversal(self,label_obj):
+		label = "<bb "+ str(label_obj.num )+">:"
+		self.statement = self.statement + " %s" %label
+	
+	def statement_traversal(self, statement):
+		try:
+			global labels
+			self.statement=self.statement + "\n"
+			assignment_statement= statement.assignment_statement_obj
+			try:
+				expression_term=assignment_statement.expression_term_obj
+				self.expression_term_traversal(expression_term)
+				self.statement = "%s = " %self.statement
+				self.onestmt="%s = " %self.onestmt
+
+
+			except:
+				pass
+			try:
+				
+				arithmetic_term=assignment_statement.arithmetic_term_obj
+				while(arithmetic_term):
+					try:
+						expression_term_obj = arithmetic_term.expression_term_obj
+						self.expression_term_traversal(expression_term_obj)
+					except:
+						pass
+			
+					try:
+						arithmetic_term = arithmetic_term.arithmetic_term_obj1 
+						flag=1
+					except:
+						break
+
+				arithmetic_term=assignment_statement.arithmetic_term_obj
+
+				while(arithmetic_term and flag):
+					try:
+						expression_term = arithmetic_term.expression_term_obj
+						self.expression_term_traversal(expression_term)
+					except:
+						pass
+			
+					try:
+						self.statement = self.statement + " %s" %arithmetic_term.bin_op
+						arithmetic_term = arithmetic_term.arithmetic_term_obj2
+					except:
+						flag=0
+						break
+			except:
+				#print(self.statement)
+				pass
+
+		except:
+			pass
+		try:
+			cond_goto=statement.cond_goto_obj
+			if_goto = cond_goto.if_goto_obj
+			self.statement = self.statement + str("if ()\n")
+			self.label_traversal(if_goto.label_obj)
+		except:
+			pass
+		try:
+			uncond_goto=statement.uncond_goto_obj
+			self.label_traversal(uncond_goto.label_obj)
+		except:
+			pass
+		try:
+			label=statement.label_obj
+			self.label_traversal(label)
+		except:
+			pass
+		try:
+			use=statement.use_obj
+			self.statement = self.statement + str("use(")
+			self.ppointer_var_traversal(use.ppointer_var_obj)
+			self.statement = self.statement + str(")")
+		except:
+			pass
+		try:
+			call_procedure_name=statement.procedure_call_obj.name
+			self.statement = self.statement + "%s" %call_procedure_name+str("(")
+			try:
+				call_parameters=statement.procedure_call_obj.call_parameters_obj
+				while(call_parameters):
+					self.ppointer_var_traversal(call_parameters.ppointer_variable_obj)
+					call_parameters=call_parameters.call_parameter_list_obj.call_parameters_obj
+					self.statement = self.statement + str(",")
+				self.statement = self.statement + str(")")
+			except:
+				self.statement = self.statement + str(")")
+		except:
+			pass
+
+	def ex_statement_list_traversal(self,ex_statement_list):
+		while(ex_statement_list):
+			try:
+				statement=ex_statement_list.statement_obj
+				self.statement_traversal(statement)
+			except:
+				pass
+			try:
+				ex_statement_list = ex_statement_list.ex_statement_list_obj
+			except:
+				pass
+
+	def var_decl_traversal(self,tree):
+		variable_declarations=tree.variable_declarations_obj	
+		variable_declaration_list=variable_declarations.variable_declaration_list_obj
+		while(variable_declaration_list):
+			self.variables='\nvar '
+			var_list=variable_declaration_list.variable_declaration_obj
+			while(var_list):
+				self.pointers='*'
+				try:
+					ppointer_variable = var_list.ppointer_variable_obj
+					self.ppointer_var_traversal(ppointer_variable)
+				except:
+					pass
+					
+				try:
+					self.variables = self.variables + var_list.variable_obj.name						
+				except:
+					pass
+
+				try:	
+					var_list=var_list.var_list_obj
+					self.variables=self.variables + ' '
+				except:
+					break					
+
+				
+			try:
+				print(self.variables)
+				variable_declaration_list=variable_declaration_list.variable_declaration_list_obj	
+			except:
+				break
+		
+	def procedure_def_traversal(self,tree):
+		procedure_defination_list = tree.procedure_defination_list_obj
+		while(procedure_defination_list):
+			try:
+				procedure_defination = procedure_defination_list.procedure_defination_obj
+				self.statement=self.statement +"%s" %procedure_defination.name
+				self.statement=self.statement + str("(")
+			except:
+				print("EXCEPT 1")
+				pass
+			try:
+				def_parameters = procedure_defination_list.procedure_defination_obj.def_parameters_obj
+				self.def_parameters_traversal(def_parameters)
+				self.statement = self.statement + str("){")
+			except:
+				pass
+			try:
+				ex_statement_list = procedure_defination_list.procedure_defination_obj.ex_statement_list_obj
+				self.ex_statement_list_traversal(ex_statement_list)
+			except:
+				print("EXCEPT 2")
+				pass
+			try:
+				ret_statement=procedure_defination_list.procedure_defination_obj.return_stat_obj.ppointer_var_obj
+				self.statement=self.statement+str("\nreturn");
+				self.ppointer_var_traversal(ret_statement)
+				
+			except:
+				pass
+			try:
+				ret_statement=procedure_defination_list.procedure_defination_obj.return_stat_obj.variable_obj.name
+				self.statement=self.statement+str("\nreturn %s " %ret_statement)
+			except:
+				pass
+			try:
+				ret_statement=procedure_defination_list.procedure_defination_obj.return_stat_obj.variable_obj.variable_obj.name
+				self.statement=self.statement+str("\nreturn  &%s " %ret_statement)
+			except:
+				
+				pass
+			try:
+				print(self.statement)
+				print ("}\n")
+				self.statement=""
+				procedure_defination_list=procedure_defination_list.procedure_defination_list_obj
+			except:
+				print("EXCEPT 3")
+				break
+			
+
+	def __init__(self,tree):
+		self.pointers='*'
+		self.variables='\nvar'
+		self.statement=""
+		self.onestmt=""
+		self.flag_p=0
+		self.var_decl_traversal(tree)
+		self.flag_p=1
+		self.procedure_def_traversal(tree)
+		self.o_flag=0
